f07ad1e826ead7de61bc6880fc3269e2839bfc1b
katejoyreed <katejoyreed@gmail.com>
2021-03-30 12:29:42 -0500

    Completed setup with necessary configuration per tutorial and added Song model and Music Controller

--- MusicLibraryWebAPI/Data/ApplicationDbContext.cs
+++ MusicLibraryWebAPI/Data/ApplicationDbContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using MusicLibraryWebAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MusicLibraryWebAPI.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions options) 
+            : base(options)
+        {
+           
+        }
+        public DbSet<Song> Songs { get; set; }
     }
 }
